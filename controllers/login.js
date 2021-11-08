21847e1e560b21a937aa1a4a79344ec6cf633fec
rajsriselvan-ca <73290591+rajsriselvan-ca@users.noreply.github.com>
2021-11-08 23:25:35 +0530

    user table modification

--- controllers/login.js
+++ controllers/login.js
@@ -2,11 +2,12 @@ const config = require("../db_config");
 const connection =  config.connection;
 
 exports.userRegister = (request, response) => {
-    const username = request.body.username;
-    const password = request.body.password;
+    const user_name = request.body.username;
+    const user_password = request.body.password;
+    const submission_date = request.body.submission_date;
 
-    connection.query(`INSERT INTO registered_users (username, user_password) VALUES
-    ("${username}", "${password}")`,
+    connection.query(`INSERT INTO registered_users (user_name, user_password, submission_date) VALUES
+    ("${user_name}", "${user_password}", "${submission_date}")`,
         (err, result) => {
             console.log("err-->>", err);
         });
