c5364801c4d6179c1557adf137b92853aacdbb65
rajsriselvan-ca <73290591+rajsriselvan-ca@users.noreply.github.com>
2021-11-28 22:15:29 +0530

    post cleanup

--- controllers/dashboard-controller.js
+++ controllers/dashboard-controller.js
@@ -17,13 +17,6 @@ exports.getLanguage = (request, response) => {
 }
 exports.createNotes = (request, response) => {
     const data = request.body;
-    var post = {
-        note_type_id: data.note_type_id,
-        program_id: data.program_id,
-        note_title: data.note_title,
-        content: data.content,
-        submission_date: data.submission_date
-    };
     connection.query(`INSERT INTO notes (note_type_id, program_id, note_title, content, submission_date ) VALUES
     ("${data.note_type_id}", "${data.program_id}", "${data.note_title}", "${data.content}", "${data.submission_date}")`,
         function (error, result) {
