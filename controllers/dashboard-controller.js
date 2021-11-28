2bb02e96a4a72cd502505d79da1e06ab03764cc4
rajsriselvan-ca <73290591+rajsriselvan-ca@users.noreply.github.com>
2021-11-28 22:14:15 +0530

    post api for notes

--- controllers/dashboard-controller.js
+++ controllers/dashboard-controller.js
@@ -1,17 +1,33 @@
 const config = require("../db_config");
-const connection =  config.connection;
+const connection = config.connection;
 
 exports.getNotesType = (request, response) => {
-    connection.query("select * from notes_type", 
-    (error, result) => {
-        if(error) response.send("error");
-        else response.send(result);
-    })
+    connection.query("select * from notes_type",
+        (error, result) => {
+            if (error) response.send("error");
+            else response.send(result);
+        })
 }
 exports.getLanguage = (request, response) => {
-    connection.query("select * from programming_language", 
-    (error, result) => {
-        if(error) response.send("error");
-        else response.send(result);
-    })
+    connection.query("select * from programming_language",
+        (error, result) => {
+            if (error) response.send("error");
+            else response.send(result);
+        })
+}
+exports.createNotes = (request, response) => {
+    const data = request.body;
+    var post = {
+        note_type_id: data.note_type_id,
+        program_id: data.program_id,
+        note_title: data.note_title,
+        content: data.content,
+        submission_date: data.submission_date
+    };
+    connection.query(`INSERT INTO notes (note_type_id, program_id, note_title, content, submission_date ) VALUES
+    ("${data.note_type_id}", "${data.program_id}", "${data.note_title}", "${data.content}", "${data.submission_date}")`,
+        function (error, result) {
+            if (error) response.send(error);
+            else response.send(result);
+        })
 }
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -4,5 +4,6 @@ dashboard = require('../controllers/dashboard-controller');
 
 router.get('/notes-type', dashboard.getNotesType);
 router.get('/get-program-language', dashboard.getLanguage);
+router.post('/create-notes', dashboard.createNotes);
 
 module.exports = router;
