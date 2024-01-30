85c28cfdeb89a56814262fca78537e2d2e0d31d5
unknown <rajsriselvan.ca@gmail.com>
2024-01-30 14:24:42 +0000

    test cockroach db config

--- controllers/dashboard-controller.js
+++ controllers/dashboard-controller.js
@@ -1,24 +1,48 @@
 const config = require("../db_config");
 const connection = config.connection;
 
+const { Client } = require("pg");
+
+const client = new Client(process.env.DATABASE_URL);
+
+client.query(`INSERT INTO notes (note_type_id, user_id, program_id, note_title, content, submission_date ) VALUES
+('23', '1', '33', 'Test Record', 'This is a test data.', '04-01-2022 01:49 AM')`,
+ (error, result) => {
+    if (error) {
+      console.error('Error executing query', error);
+    } else {
+      console.log('Result:', result.rows);
+    }})
+
+exports.createNotes = (request, response) => {
+    const data = request.body;
+    const incomingContent = JSON.stringify(data.content);
+    client.query(`INSERT INTO notes (note_type_id, user_id, program_id, note_title, content, submission_date ) VALUES
+    ("${data.note_type_id}", "${data.user_id}", "${data.program_id}", "${data.note_title}", ${incomingContent}, "${data.submission_date}")`,
+        function (error, result) {
+            if (error) response.send(error);
+            else response.send("success");
+        })
+}
+
 exports.getNotesType = (request, response) => {
-    connection.query("select * from notes_type",
+    client.query("select * from notes_type",
         (error, result) => {
             if (error) response.send(error);
-            else response.send(result);
+            else response.send(result.rows);
         })
 }
 exports.getLanguage = (request, response) => {
-    connection.query("select * from programming_language",
+    client.query("select * from programming_language",
         (error, result) => {
             if (error) response.send(error);
-            else response.send(result);
+            else response.send(result.rows);
         })
 }
 exports.createNotes = (request, response) => {
     const data = request.body;
     const incomingContent = JSON.stringify(data.content);
-    connection.query(`INSERT INTO notes (note_type_id, user_id, program_id, note_title, content, submission_date ) VALUES
+    client.query(`INSERT INTO notes (note_type_id, user_id, program_id, note_title, content, submission_date ) VALUES
     ("${data.note_type_id}", "${data.user_id}", "${data.program_id}", "${data.note_title}", ${incomingContent}, "${data.submission_date}")`,
         function (error, result) {
             if (error) response.send(error);
@@ -27,23 +51,23 @@ exports.createNotes = (request, response) => {
 }
 exports.getNotes = (request, response) => {
     const data = request.query;
-    connection.query(`select * from notes where user_id = "${data.user_id}" and note_type_id = "${data.selectedTab}"`,
+    client.query(`select * from notes where user_id = "${data.user_id}" and note_type_id = "${data.selectedTab}"`,
         (error, result) => {
             if (error) response.send(error);
-            else response.send(result);
+            else response.send(result.rows);
         })
 }
 exports.getAllNotes = (request, response) => {
      const data = request.query;
-     connection.query(`select * from notes where user_id = "${data.user_id}"`,
+     client.query(`select * from notes where user_id = "${data.user_id}"`,
          (error, result) => {
              if (error) response.send(error);
-             else response.send(result);
+             else response.send(result.rows);
          })
  }
 exports.deleteNote = (request, response) => {
     const id = request.params.id;
-     connection.query(`delete from notes where note_id = "${id}"`,
+    client.query(`delete from notes where note_id = "${id}"`,
          (error, result) => {
              if (error) response.send(error);
              else response.send("success");
@@ -53,10 +77,11 @@ exports.deleteNote = (request, response) => {
     const data = request.body;
     const incomingContent = JSON.stringify(data.content);
     const id = JSON.parse(request.params.id)
-     connection.query(`update notes set note_title = "${data.note_title}", content = ${incomingContent}, user_id = "${data.user_id}",
+    client.query(`update notes set note_title = "${data.note_title}", content = ${incomingContent}, user_id = "${data.user_id}",
      note_type_id = "${data.note_type_id}", program_id = "${data.program_id}" where note_id = ${id}`,
          (error, result) => {
              if (error) response.send(error);
              else response.send("success");
          })
+         client.end();
  }
--- controllers/login-controller.js
+++ controllers/login-controller.js
@@ -2,18 +2,42 @@ const config = require("../db_config");
 const connection =  config.connection;
 const jwt = require('jsonwebtoken');
 
+const { Client } = require("pg");
+
+const client = new Client(process.env.DATABASE_URL);
+
+client.connect();
+
+// client.query(`INSERT INTO  (language_id, language_name ) VALUES
+// ('8','Other')`,
+//  (error, result) => {
+//     if (error) {
+//       console.error('Error executing query', error);
+//     } else {
+//       console.log('Result:', result);
+//     }}) 
+
+client.query("select * from programming_language", (error, result) => {
+  if (error) {
+    console.error('Error executing query', error);
+  } else {
+    console.log('Result:', result.rows);
+  }
+  
+});
+
 exports.userRegister = (request, response) => {
     const user_name = request.body.username;
     const user_password = request.body.password;
     const submission_date = request.body.submission_date;
 
-    connection.query("select * from registered_users",
+    client.query("select * from registered_users",
     (error, registeredUsers) => {
         if(error) response.send(error);
         else {
             const UserExist = registeredUsers.find(record => record.user_name.toLowerCase() === user_name.toLowerCase()) === undefined ? false : true;
             if(!UserExist) {
-                connection.query(`INSERT INTO registered_users (user_name, user_password, submission_date) VALUES
+                client.query(`INSERT INTO registered_users (user_name, user_password, submission_date) VALUES
                 ("${user_name}", "${user_password}", "${submission_date}")`,
                     (error, result) => {
                         if(error) response.send(error);
@@ -29,17 +53,18 @@ exports.userRegister = (request, response) => {
 exports.loginUserDetailsPost = (request, response) => {
     const user_name = request.body.username;
     const user_password = request.body.password;
-    connection.query(`select user_id, user_name, submission_date from registered_users where user_name = "${user_name}" AND user_password = "${user_password}"`,
+    client.query(`select user_id, user_name, submission_date from registered_users where user_name = "${user_name}" AND user_password = "${user_password}"`,
     (error, userList) => {
         if(error) response.send(error);
         else {
             const user = { name: user_name};
-            if(userList.length){
+            if(userList.rows.length){
                 const jwtToken = jwt.sign(user, process.env.JWT_ACCESS_KEY, { expiresIn: '10m'});
-                response.json({token: jwtToken, userDetails: userList[0]});
+                response.json({token: jwtToken, userDetails: userList.rows[0]});
             } else {
                 response.send("User Not Exist");    
             }
         }
     });
+    client.end();
 };
