fdcb7653ad02266f4d97565c142bd9e9c7a07e96
unknown <rajsriselvan.ca@gmail.com>
2024-02-05 01:18:46 +0000

    Console cleanup

--- controllers/login-controller.js
+++ controllers/login-controller.js
@@ -37,13 +37,11 @@ exports.userRegister = (request, response) => {
 exports.loginUserDetailsPost = (request, response) => {
     const user_name = request.body.username;
     const user_password = request.body.password;
-    console.log("heyy herere loginn---",user_name, user_password);
     client.query(`select user_id, user_name, submission_date from registered_users where user_name = '${user_name}' AND user_password = '${user_password}'`,
     (error, userList) => {
         if(error) response.send(error);
         else {
             const user = { name: user_name};
-            console.log("22n---",userList.rows);
             if(userList.rows.length){
                 const jwtToken = jwt.sign(user, process.env.JWT_ACCESS_KEY, { expiresIn: '10m'});
                 response.json({token: jwtToken, userDetails: userList.rows[0]});
