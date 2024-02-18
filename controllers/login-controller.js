3930f12352f67a58173aeddcaf841385c2a438da
unknown <rajsriselvan.ca@gmail.com>
2024-02-18 16:57:49 +0000

    Login,register query sanitize

--- controllers/login-controller.js
+++ controllers/login-controller.js
@@ -8,27 +8,29 @@ const client = new Client(process.env.DATABASE_URL);
 
 client.connect();
 
-
-
 exports.userRegister = (request, response) => {
     const user_name = request.body.username;
     const user_password = request.body.password;
     const submission_date = request.body.submission_date;
 
-    client.query("select * from registered_users",
+    client.query("SELECT * FROM registered_users WHERE LOWER(user_name) = LOWER($1)", [user_name],
     (error, registeredUsers) => {
-        if(error) response.send(error);
-        else {
-            const UserExist = registeredUsers.rows.find(record => record.user_name.toLowerCase() === user_name.toLowerCase()) === undefined ? false : true;
-            if(!UserExist) {
-                client.query(`INSERT INTO registered_users (user_name, user_password, submission_date) VALUES
-                ('${user_name}', '${user_password}', '${submission_date}')`,
-                    (error, result) => {
-                        if(error) response.send(error);
-                        else response.send("success");
-                    });
+        if(error) {
+            response.status(500).send(error);
+        } else {
+            const userExists = registeredUsers.length > 0;
+            if(!userExists) {
+                const query = "INSERT INTO registered_users (user_name, user_password, submission_date) VALUES ($1, $2, $3)";
+                const values = [user_name, user_password, submission_date];
+                connection.query(query, values, (error, result) => {
+                    if(error) {
+                        response.status(500).send(error);
+                    } else {
+                        response.status(200).send("success");
+                    }
+                });
             } else {
-                response.send("error");
+                response.status(400).send("User already exists");
             }
         }
     });
@@ -37,17 +39,22 @@ exports.userRegister = (request, response) => {
 exports.loginUserDetailsPost = (request, response) => {
     const user_name = request.body.username;
     const user_password = request.body.password;
-    client.query(`select user_id, user_name, submission_date from registered_users where user_name = '${user_name}' AND user_password = '${user_password}'`,
-    (error, userList) => {
-        if(error) response.send(error);
-        else {
-            const user = { name: user_name};
-            if(userList.rows.length){
-                const jwtToken = jwt.sign(user, process.env.JWT_ACCESS_KEY, { expiresIn: '10m'});
-                response.json({token: jwtToken, userDetails: userList.rows[0]});
+
+    client.query(
+        "SELECT user_id, user_name, submission_date FROM registered_users WHERE user_name = $1 AND user_password = $2",
+        [user_name, user_password],
+        (error, userList) => {
+            if (error) {
+                response.status(500).send(error);
             } else {
-                response.send("User Not Exist");    
+                const user = { name: user_name };
+                if (userList.length) {
+                    const jwtToken = jwt.sign(user, process.env.JWT_ACCESS_KEY, { expiresIn: '90m' });
+                    response.json({ token: jwtToken, userDetails: userList[0] });
+                } else {
+                    response.status(400).send("User Not Exist");
+                }
             }
         }
-    });
+    );
 };
