0bff9d67df2fb9b068aefff0234b9e642ac9adb2
unknown <rajsriselvan.ca@gmail.com>
2024-02-18 16:28:45 +0000

    create note query sanitize

--- controllers/dashboard-controller.js
+++ controllers/dashboard-controller.js
@@ -35,12 +35,15 @@ exports.getLanguage = (request, response) => {
 exports.createNotes = (request, response) => {
     const data = request.body;
     const incomingContent = data.content;
-    client.query(`INSERT INTO notes (note_type_id, user_id, program_id, note_title, content, submission_date ) VALUES
-    ('${data.note_type_id}', '${data.user_id}', '${data.program_id}', '${data.note_title}', '${incomingContent}', '${data.submission_date}')`,
-        function (error, result) {
-            if (error) response.send(error);
-            else response.send("success");
-        })
+    const query = `INSERT INTO notes (note_type_id, user_id, program_id, note_title, content, submission_date ) VALUES ($1, $2, $3, $4, $5, $6)`;
+    const values = [data.note_type_id, data.user_id, data.program_id, data.note_title, incomingContent, data.submission_date];
+    connection.query(query, values, function (error, result) {
+        if (error) {
+            response.status(500).send(error);
+        } else {
+            response.status(200).send("success");
+        }
+    });
 }
 exports.getNotes = (request, response) => {
     const data = request.query;
